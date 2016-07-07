a283771e2b88c319cb071524fa67d80eea103681
Yvonnick Esnault <yvonnick.esnault@ovh.net>
2016-07-07 14:44:18 +0200

    feat: new route for remove msg
    Signed-off-by: Yvonnick Esnault <yvonnick.esnault@ovh.net>

--- releaseview/list/releaseview-list.directive.js
+++ releaseview/list/releaseview-list.directive.js
@@ -70,7 +70,7 @@ angular.module('TatUi').directive('messagesReleaseviewItem', function($compile) 
        * @description delete a message from a Private topic
        */
       this.deleteMessage = function(message) {
-        TatEngineMessagesRsc.delete({
+        TatEngineMessageRsc.delete({
           'idMessageToDelete': message._id,
           'cascade': 'cascade/'
         }).$promise.then(function(resp) {
